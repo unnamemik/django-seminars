0cdc4c94197455ee78ba5181eb722f078aa6669c
Mikael <unnamemik@gmail.com>
2023-10-03 22:57:13 +0300

    seminar5 homework

--- seminar5/admin.py
+++ seminar5/admin.py
@@ -9,6 +9,11 @@ def reset_quant(modeladmin, request, queryset):
     queryset.update(showed=0)
 
 
+@admin.action(description='Обнулить количество')
+def reset_prod_quant(modeladmin, request, queryset):
+    queryset.update(prod_quant=0)
+
+
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'author', 'showed']
     ordering = ['-showed']
@@ -28,9 +33,18 @@ class PostAdmin(admin.ModelAdmin):
     ]
 
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'prod_quant']
+    ordering = ['-price']
+    search_fields = ['description']
+    list_filter = ['reg_date']
+    list_per_page = 10
+    actions = [reset_prod_quant]
+
+
 admin.site.register(HeadsTails)
 admin.site.register(User)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Order)
 admin.site.register(Author)
 admin.site.register(Post, PostAdmin)
